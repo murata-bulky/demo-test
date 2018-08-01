4bc6d45800486cf3958f0bcb698f6844c82e3b63
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2018-08-01 19:46:39 +0900

    Add files via upload

--- testDB.php
+++ testDB.php
@@ -2,7 +2,7 @@
 
 try {
 	// DBへ接続
-	$dbh = new PDO("pgsql:host=ec2-50-16-241-91.compute-1.amazonaws.co; dbname=dc9ftkn51cgcj;", 'zhyyeuduffaiuf', 'b770c8d49644c40a7bbf73401a7c8dd8e140829b3ae028b70fe7e35067279898');
+	$dbh = new PDO("pgsql:host=ec2-50-16-241-91.compute-1.amazonaws.com; dbname=dc9ftkn51cgcj;", 'zhyyeuduffaiuf', 'b770c8d49644c40a7bbf73401a7c8dd8e140829b3ae028b70fe7e35067279898');
 
 	// testテーブルにデータを登録
 	$sql = "insert test (name) values ('test')";
@@ -17,12 +17,12 @@ try {
 	print_r("実行");
 	
 	foreach($data as $value){
-		var_dump($value['name']);
+		var_dump("氏名".$value['name']);
 		//error_log($value['name']);
 	}
 
 } catch(PDOException $e) {
-	print_r($e->getMessage());
+	print_r("エラー".$e->getMessage());
 	error_log($e->getMessage());
 	die();
 }
