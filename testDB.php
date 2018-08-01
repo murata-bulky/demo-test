b586551434b076f1d0129489b9ecadd2ef516f86
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2018-08-01 18:34:30 +0900

    Add files via upload
    create文修正

--- testDB.php
+++ testDB.php
@@ -5,15 +5,15 @@ try {
 	$dbh = new PDO("pgsql:host=ec2-50-16-241-91.compute-1.amazonaws.com; dbname=dc9ftkn51cgcj;", 'zhyyeuduffaiuf', 'b770c8d49644c40a7bbf73401a7c8dd8e140829b3ae028b70fe7e35067279898');
 
 	// SQL作成
-	$sql = 'CREATE TABLE user (
-		id INT(11) AUTO_INCREMENT PRIMARY KEY,
-		name VARCHAR(20),
-		age INT(11),
-		registry_datetime DATETIME
-	) engine=innodb default charset=utf8';
+	$sql = "CREATE TABLE user_list (
+	id serial PRIMARY KEY,
+	name varchar(50),
+	age int,
+	registry_datetime timestamp)";
 
 	// SQL実行
 	$res = $dbh->query($sql);
+	print_r($res);
 	
 	// testテーブルにデータを登録
 	$sql = 'insert into user (id,name) values ("12345678902","test")';
