54006b8ac84f8955b0074689b5df491b0bda1fa8
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2018-08-01 18:38:38 +0900

    Add files via upload
    修正

--- testDB.php
+++ testDB.php
@@ -5,7 +5,7 @@ try {
 	$dbh = new PDO("pgsql:host=ec2-50-16-241-91.compute-1.amazonaws.com; dbname=dc9ftkn51cgcj;", 'zhyyeuduffaiuf', 'b770c8d49644c40a7bbf73401a7c8dd8e140829b3ae028b70fe7e35067279898');
 
 	// SQLçÏê¨
-	$sql = "CREATE TABLE user_list (
+	$sql = "CREATE TABLE user (
 	id serial PRIMARY KEY,
 	name varchar(50),
 	age int,
