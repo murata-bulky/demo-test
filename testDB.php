88b5097063330232ddec1e9c1dfea89a3c422b5f
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2018-08-01 19:06:25 +0900

    Add files via upload
    修正

--- testDB.php
+++ testDB.php
@@ -4,32 +4,20 @@ try {
 	// DBへ接続
 	$dbh = new PDO("pgsql:host=ec2-50-16-241-91.compute-1.amazonaws.com; dbname=dc9ftkn51cgcj;", 'zhyyeuduffaiuf', 'b770c8d49644c40a7bbf73401a7c8dd8e140829b3ae028b70fe7e35067279898');
 
-	// SQL作成
-	/*
-	$sql = "CREATE TABLE user_list (
-	id serial PRIMARY KEY,
-	name varchar(50),
-	age int,
-	registry_datetime timestamp)";
-
-	// SQL実行
-	$res = $dbh->query($sql);
-	print_r($res);
-	*/
 	// testテーブルにデータを登録
-	$sql = 'insert into user_list (id,name) values ("12345678902","test")';
+	$sql = "insert test (id,name) values ('12345678901','test')";
 	
 	// SQL実行
 	$res = $dbh->query($sql);
-	print_r($res);
+	print_r("insert".$res);
 	
 	// testテーブルの全データを取得
-	$sql = 'SELECT * FROM user_list';
+	$sql = "SELECT * FROM test";
 	$data = $dbh->query($sql);
 
 	if( !empty($data) ) {
 		foreach( $data as $value ) {
-			print_r($value['name']);
+			print_r("氏名".$value['name']);
 			error_log($value['name']);
 		}
 	}
