f751d9322ed644314cd2f66cc2bdba7696d01e82
bulkyMikuni <41996251+bulkyMikuni@users.noreply.github.com>
2018-08-30 20:02:53 +0900

    Add files via upload

--- pushmessage.php
+++ pushmessage.php
@@ -1,19 +1,10 @@
 <?php
 
-$aaa = "sasasasa";
-
-echo $aaa;
-
-
 // receive json data from line webhook
 $jsonString = file_get_contents('php://input');
 error_log('テスト１');
 $jsonObj = json_decode($jsonString, true);
 error_log($jsonObj);
-
-echo "テスト１";
-echo $jsonObj;
-
 
 
 $message_text = $jsonObj["message"];
@@ -21,14 +12,10 @@ $sendID = $jsonObj["sendID"];
 error_log($message_text);
 error_log($sendID);
 
-echo $message_text;
-echo $sendID;
-
-
 //ファイルに吐き出す
 //ファイル出力
 $json = fopen('./QandA.json', 'w+b');
-fwrite($json, json_encode($post_data));
+fwrite($json, json_encode($jsonObj));
 fclose($json);
 
 ?>
