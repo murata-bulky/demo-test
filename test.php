818d86405a53e78194a88c5c4dc8aafe38bd4e04
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2018-09-12 22:21:21 +0900

    Add files via upload

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 	//watsonへの送信データ
 	//$data = array("input" => array("text" => $_POST["inquiry"]));
-	//$data = array("intent" => "新商品","examples" => array("text" => "新商品について教えてください","text" => "新商品について"));
-	$data = array("intent" => "新商品");
+	$data = array("intent" => "新商品2","examples" => array("text" => "新商品について教えてください","text" => "新商品について"));
+	//$data = array("intent" => "新商品");
 	
 	//watson接続URL
 	$url = 'https://gateway.watsonplatform.net/assistant/api/v1/workspaces/d77632f7-8c14-4076-8512-3b1760cf3841/intents?version=2018-07-10';
@@ -10,7 +10,7 @@
 	//watsonのユーザーID : パスワード
 	$userInfo = '69bcc66a-a892-438e-b7f7-ec7af5014788:2vYLXsmtFGbg';
 	
-	$encode = json_encode($data);
+	$encode = json_encode($data,JSON_FORCE_OBJECT);
 	print_r("確認".$encode);
 	//送信用のCURLコマンド作成
 	$ch = curl_init($url);
